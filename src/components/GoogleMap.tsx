39ce791d3c24903bb4930c81561b4aaebd7720d3
Kingsley Munachi <info@mymuna.store>
2025-09-29 08:18:48 -0400

    Fix Google Maps API Key Error

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -166,7 +166,7 @@ const GoogleMap = ({ className = "" }: GoogleMapProps) => {
 
     // Load Google Maps API
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&callback=initMap&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&callback=initMap&libraries=places`;
     script.async = true;
     script.defer = true;
     
